57ef1548116742d3bd22f727a0fe3697db64d3a8
Marc Neudert <marc.neudert@gmail.com>
2012-09-13 23:12:44 +0200

    displays HEAD commit message with corresponding SHA on index

--- src/elgit_yaws.erl
+++ src/elgit_yaws.erl
@@ -1,6 +1,7 @@
 -module(elgit_yaws).
 -export ([out/1]).
 
+-include_lib("gert/include/gert.hrl").
 -include_lib("yaws/include/yaws_api.hrl").
 
 out(Arg) ->
@@ -14,15 +15,32 @@ out(Arg) ->
     end.
 
 out_index(Arg) ->
-    HeadSha = gert:get_head_sha(Arg#arg.docroot ++ "/.git"),
+    RepoPath = Arg#arg.docroot ++ "/.git",
+    HeadSha = gert:get_head_sha(RepoPath),
+    HeadCommit = gert:get_commit_record(RepoPath, HeadSha),
+    HeadCommitMsg = HeadCommit#commit.message,
     [{html, [<<"
 <html>
     <head>
+        <link href=\"http://static.elgit.dev/css/bootstrap.css\" rel=\"stylesheet\"/>
+        <link href=\"http://static.elgit.dev/css/bootstrap-responsive.css\" rel=\"stylesheet\">
         <title>El Git</title>
     </head>
     <body>
+        <div class=\"navbar\">
+            <div class=\"navbar-inner\">
+                <span class=\"brand\" href=\"#\">El Git</span>
+                <ul class=\"nav\">
+                    <li class=\"active\"><a href=\"#\">Home</a></li>
+                </ul>
+            </div>
+        </div>
+
         <h1>Hello World!</h1>
-        <h2>HEAD: ">>, HeadSha, <<"</h2>
+        <h2>">>, HeadCommitMsg, <<" @ ">>, HeadSha, <<"</h2>
+
+        <script src=\"http://static.elgit.dev/js/jquery.js\"></script>
+        <script src=\"http://static.elgit.dev/js/bootstrap.js\"></script>
     </body>
 </html>
 ">>]}].
