8e1a255698010e1343b7fef4ea85bf84265f1422
Marc Neudert <marc.neudert@gmail.com>
2012-09-23 22:19:13 +0200

    prepares dual xhr-nonxhr-mode

--- src/elgit_repo.erl
+++ src/elgit_repo.erl
@@ -4,11 +4,19 @@
 -include_lib("yaws_api.hrl").
 
 out(Arg) ->
+    case yaws_api:queryvar(Arg, "partial") of
+        {ok, _} ->
+            out_partial(Arg);
+        undefined ->
+            [elgit_www:header(Arg),
+             out_partial(Arg),
+             elgit_www:footer(Arg)]
+    end.
+
+out_partial(Arg) ->
     Req = Arg#arg.req,
     {abs_path, Path} = Req#http_request.path,
     Repo = elgit_shared:get_repo(Arg, string:substr(Path, 2)),
-    [elgit_www:header(Arg),
-     {html, [<<"
+    {html, [<<"
 <script data-main=\"/js/elgit.js\" src=\"/js/lib/require.js\"></script>
-">>]},
-     elgit_www:footer(Arg)].
+">>]}.
