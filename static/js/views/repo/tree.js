7d6124b95cb40415bf4a8e3a4da65d1244f333c5
Marc Neudert <marc.neudert@gmail.com>
2012-10-04 18:12:27 +0200

    adds partial reload to repo tree breadcrumb

--- static/js/views/repo/tree.js
+++ static/js/views/repo/tree.js
@@ -7,19 +7,29 @@ define([
         render: function(repo, oid, path) {
             var view = this;
 
+            $('#tree-crumb a').each(function(index) {
+                treeParts = $(this).attr('href').match(/^\/.+\/tree\/.+?\/(.*)$/);
+
+                $(this).click(function() {
+                    view.changeTree(repo, oid, treeParts[1]);
+                    return false;
+                });
+            });
+
             $('#tree .tree a').each(function(index) {
                 $(this).click(function() {
-                    view.changeTree(repo, oid, path, this);
+                    view.changeTree(repo, oid, path + this.text + '/');
                     return false;
                 });
             });
         },
 
-        changeTree: function(repo, oid, path, treeAnchor) {
+        changeTree: function(repo, oid, path) {
+            console.log('onclick',path);
             var view = this;
             var treeLink = '/' + repo +
                            '/tree/' + oid +
-                           '/' + path + treeAnchor.text + '/';
+                           '/' + path;
 
             $.ajax({
                 url: treeLink + '?partial=1',
@@ -27,7 +37,7 @@ define([
                     $('#repo').html(response);
 
                     Backbone.history.navigate(treeLink, true);
-                    view.render(repo, oid, path + treeAnchor.text + '/');
+                    view.render(repo, oid, path);
                 }
             });
         }
